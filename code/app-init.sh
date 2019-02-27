4784dd0542685b02fcbc228b924e6dff83592b8d
rocha <thiagorochafortal@gmail.com>
2019-02-27 14:50:17 -0300

    ajustes arquivos do docker; permissoes

---
+++ code/app-init.sh
@@ -0,0 +1,15 @@
+composer install -d ./app
+
+mkdir -p ./app/storage/app/public/images
+
+php ./app/artisan migrate --seed
+
+# php ./app/artisan key:generate
+
+php ./app/artisan passport:install
+
+composer install -d ./admin
+
+php ./app/artisan key:generate
+
+php-fpm
