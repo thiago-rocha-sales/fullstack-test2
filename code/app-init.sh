63b14948987710422e1c99c4d9076af68caf23d3
Thiago Rocha <thiagorochafortal@gmail.com>
2019-02-27 21:06:29 -0300

    ajustes nas permissoes

--- code/app-init.sh
+++ code/app-init.sh
@@ -2,12 +2,20 @@ composer install -d ./app
 
 mkdir -p ./app/storage/app/public/images
 
+mkdir -p ./app/storage/framework/cache \
+    ./app/storage/framework/sessions \
+    ./app/storage/framework/views
+
+mkdir -p ./admin/storage/framework/cache \
+    ./admin/storage/framework/sessions \
+    ./admin/storage/framework/views
+
 php ./app/artisan migrate --seed
 
 php ./app/artisan passport:install
 
 composer install -d ./admin
 
-php ./app/artisan key:generate
+# php ./app/artisan key:generate
 
-php-fpm
+# php-fpm
